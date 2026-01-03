9cf2d705d28f8f47c0a0519975174e12fda1eb2e
saleem <ab588538@gmail.com>
2026-01-03 22:23:08 +0530

    update the routes

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,13 @@ const router = express.Router();
 
 // Import module routes here
 import pdfRoutes from "./modules/pdf/pdf.routes.js";
-router.use("/pdf", pdfRoutes);
-
 import paymentRoutes from "./modules/payment/payment.routes.js";
-router.use("/payment", paymentRoutes);
-
 import userRoutes from "./modules/users/user.routes.js";
-router.use("/users", userRoutes);
-
 import courseRoutes from "./modules/course/course.routes.js";
+
+router.use("/pdf", pdfRoutes);
+router.use("/payment", paymentRoutes);
+router.use("/users", userRoutes);
 router.use("/courses",courseRoutes)
 
 export default router;
